194cc068e866cf3595264a3dcd66295e66eb2320
rabindr718 <rabindraksharma19@gmail.com>
2024-02-11 15:52:21 +0530

    Some files are deleted

--- Practice/src/basic/Factorial.java
+++ Practice/src/basic/Factorial.java
@@ -3,16 +3,13 @@ package basic;
 public class Factorial {
 
 	public static void main(String[] args) {
-int fact=1;
-int num=5;
-int i=1;
-while(i<=num) {
-	fact=fact*i;
-
-	i++;
-}
-System.out.print(fact+num);
-//System.out.println(fact);
+		int n=1,num=5;
+		for(int i=1; i<=num; i++ ) {
+			n=n*i;
+		}
+		
+		
+		System.out.print(n);
 
 	}
 
